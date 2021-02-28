efdab533f821b59f11e420cf6c08e75939fcd5ac
sasikumar6795 <32613154+sasikumar6795@users.noreply.github.com>
2021-02-28 18:13:12 +0530

    github service

--- github-login-firebase/src/app/services/github.service.ts
+++ github-login-firebase/src/app/services/github.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+
+//https://api.github.com/users/sasikumar6795
 @Injectable({
   providedIn: 'root'
 })
 export class GithubService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getUserDetails(userName: string)
+  {
+    return this.http.get(`https://api.github.com/users/${userName}`);
+  }
+
+  getRepos(repoUrl: string)
+  {
+    return this.http.get(repoUrl);
+  }
 }
