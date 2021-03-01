a9d4c41d3f443af8d58af9beb69df1a4aefd498b
sasikumar6795 <32613154+sasikumar6795@users.noreply.github.com>
2021-03-01 19:25:31 +0530

    header template

--- github-login-firebase/src/app/layout/header/header.component.ts
+++ github-login-firebase/src/app/layout/header/header.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from 'src/app/services/auth.service';
+
 
 @Component({
   selector: 'app-header',
@@ -7,9 +11,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+ email : any;
+
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+    private toastr: ToastrService 
+    )
+    {
+        auth.getUser().subscribe(
+          (user) => {
+            //email property might not be present in the user object thats why question mark
+            this.email = user?.email;
+          });
+    }
 
   ngOnInit(): void {
+  }
+
+  async signOut()
+  {
+    try {
+      const response = await this.auth.signOut();
+      this.router.navigateByUrl('/signIn');
+      this.toastr.info("login again to continue");
+      this.email=null;
+    } catch (error) {
+      this.toastr.error('something went wrong');
+    }
   }
 
 }
