047a86ba01e04c2c0c7e7438bd3176f4fed78848
sasikumar6795 <32613154+sasikumar6795@users.noreply.github.com>
2021-03-01 20:15:23 +0530

    detect changes in grand child

--- github-login-firebase/src/app/components/repos/repos.component.ts
+++ github-login-firebase/src/app/components/repos/repos.component.ts
@@ -1,13 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, ChangeDetectorRef, Input} from '@angular/core';
+import { GithubService } from 'src/app/services/github.service';
+
 
 @Component({
   selector: 'app-repos',
   templateUrl: './repos.component.html',
   styleUrls: ['./repos.component.css']
 })
-export class ReposComponent implements OnInit {
+export class ReposComponent implements OnInit, OnChanges {
 
-  constructor() { }
+  @Input()
+  repoUrl!: string;
+
+  repos: [] = [];
+
+
+
+  constructor(
+    private githubService:GithubService,
+    private changeDetectorRef: ChangeDetectorRef
+  ) { }
+
+
+  ngOnChanges(){
+
+    if(this.repoUrl)
+    {
+      this.githubService.getRepos(this.repoUrl).subscribe(
+        (repos : any) => {
+          this.repos=repos;
+
+          this.changeDetectorRef.detectChanges();
+        },
+        (err) => {
+          console.log(err);
+        }
+        
+      );
+    }
+
+  }
 
   ngOnInit(): void {
   }
