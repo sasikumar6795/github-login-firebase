3eec42aee86143fa187b8723021c427f7cf41dc7
sasikumar6795 <32613154+sasikumar6795@users.noreply.github.com>
2021-03-02 11:37:28 +0530

    align

--- github-login-firebase/src/app/layout/header/header.component.ts
+++ github-login-firebase/src/app/layout/header/header.component.ts
@@ -3,42 +3,35 @@ import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { AuthService } from 'src/app/services/auth.service';
 
-
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-
- email : any;
+  email: any;
 
   constructor(
     private auth: AuthService,
     private router: Router,
-    private toastr: ToastrService 
-    )
-    {
-        auth.getUser().subscribe(
-          (user) => {
-            //email property might not be present in the user object thats why question mark
-            this.email = user?.email;
-          });
-    }
-
-  ngOnInit(): void {
+    private toastr: ToastrService
+  ) {
+    auth.getUser().subscribe((user) => {
+      //email property might not be present in the user object thats why question mark
+      this.email = user?.email;
+    });
   }
 
-  async signOut()
-  {
+  ngOnInit(): void {}
+
+  async signOut() {
     try {
       const response = await this.auth.signOut();
       this.router.navigateByUrl('/signIn');
-      this.toastr.info("login again to continue");
-      this.email=null;
+      this.toastr.info('login again to continue');
+      this.email = null;
     } catch (error) {
       this.toastr.error('something went wrong');
     }
   }
-
 }
