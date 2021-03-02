06c5b7e620e7f8cde6f3f793554afd31a12fefac
sasikumar6795 <32613154+sasikumar6795@users.noreply.github.com>
2021-03-02 11:04:24 +0530

    usercard

--- github-login-firebase/src/app/components/repos/repos.component.ts
+++ github-login-firebase/src/app/components/repos/repos.component.ts
@@ -1,47 +1,42 @@
-import { Component, OnInit, OnChanges, ChangeDetectorRef, Input} from '@angular/core';
+import {
+  Component,
+  OnInit,
+  OnChanges,
+  ChangeDetectorRef,
+  Input,
+} from '@angular/core';
 import { GithubService } from 'src/app/services/github.service';
-
 
 @Component({
   selector: 'app-repos',
   templateUrl: './repos.component.html',
-  styleUrls: ['./repos.component.css']
+  styleUrls: ['./repos.component.css'],
 })
 export class ReposComponent implements OnInit, OnChanges {
-
   @Input()
   repoUrl!: string;
 
   repos: [] = [];
 
-
-
   constructor(
-    private githubService:GithubService,
+    private githubService: GithubService,
     private changeDetectorRef: ChangeDetectorRef
-  ) { }
+  ) {}
 
-
-  ngOnChanges(){
-
-    if(this.repoUrl)
-    {
+  ngOnChanges() {
+    if (this.repoUrl) {
       this.githubService.getRepos(this.repoUrl).subscribe(
-        (repos : any) => {
-          this.repos=repos;
+        (repos: any) => {
+          this.repos = repos;
 
           this.changeDetectorRef.detectChanges();
         },
         (err) => {
           console.log(err);
         }
-        
       );
     }
-
   }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- github-login-firebase/src/app/components/usercard/usercard.component.ts
+++ github-login-firebase/src/app/components/usercard/usercard.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-usercard',
   templateUrl: './usercard.component.html',
-  styleUrls: ['./usercard.component.css']
+  styleUrls: ['./usercard.component.css'],
 })
 export class UsercardComponent implements OnInit {
+  @Input() user: any;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
