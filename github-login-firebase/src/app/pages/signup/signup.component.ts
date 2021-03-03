83cc1d442aa6cb17b872f6b8a25242fc5639a85d
sasikumar6795 <32613154+sasikumar6795@users.noreply.github.com>
2021-03-03 12:16:05 +0530

    signup started

--- github-login-firebase/src/app/pages/signup/signup.component.ts
+++ github-login-firebase/src/app/pages/signup/signup.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import {  NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { from } from 'rxjs';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent implements OnInit {
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private toastr: ToastrService
+  ) {}
 
-  constructor() { }
 
-  ngOnInit(): void {
+  onSubmit(f : NgForm)
+  {
+    console.log(f.value);
   }
 
+  ngOnInit(): void {}
 }
